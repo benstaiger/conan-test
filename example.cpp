c9ddaf96a331aa7cb8c28ff8ed72ff528dfb5e05
bstaiger <bhstaiger@gmail.com>
2022-07-26 16:47:19 -0500

    animate

--- example.cpp
+++ example.cpp
@@ -2,6 +2,7 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+
 #include <iostream>
 
 
@@ -13,12 +14,14 @@ int main()
         std::cout << "Failed to create empty image" << std::endl;
         return 1;
     }
-    cv::rectangle(img, cv::Rect(10, 10, 100, 100), {0, 0, 200});
-    cv::imshow("Display window", img);
-    int k = cv::waitKey(0); // Wait for a keystroke in the window
-    if(k == 's')
-    {
-       cv::imwrite("example.png", img);
+    for (size_t i = 0; i < 10; ++i) {
+        cv::rectangle(img, cv::Rect(10, 10*i, 100, 100), {0, 0, 200});
+        cv::imshow("Display window", img);
+        int k = cv::waitKey(1000); // Wait for a keystroke in the window
+        if(k == 's') {
+            cv::imwrite("example.png", img);
+            break;
+        }
     }
     return 0;
 }
